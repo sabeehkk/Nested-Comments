3e699f2af5389e043165688858066f87866c60dc
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-07-20 20:00:26 +0530

    updated delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import './App.css'
 import NestedComents from './components/nested_coments'
 import ComentsData from './data/coments.json'
 function App() {
-  const handleDelete = (commentId) => {
-    console.log('Deleted comment ID:', commentId);
-    // Additional logic if needed
-  };
+ 
   return (
    <div>
     <h1>Nested coment system</h1>
@@ -14,7 +11,7 @@ function App() {
      coments={ComentsData}
      onSubmit={(content)=>{content}}
      onEdit={(content)=>{content}}
-     onDelete={handleDelete}
+     onDelete={(handleDelete)=>{handleDelete}}
      />
    </div>
   )
